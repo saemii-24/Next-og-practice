691ce6d1b09106e9dc3ccb54fa504008803f93fc
saemii-24 <hims2419@gmail.com>
2024-06-01 23:59:45 +0900

    Fix: 이미지 메타데이터 변경

--- src/app/fifth/[id]/opengraph-image.tsx
+++ src/app/fifth/[id]/opengraph-image.tsx
@@ -1,31 +1,53 @@
 import { ImageResponse } from "next/og";
 import posts from "../../../data.json";
 
-//https://nextjs.org/docs/app/api-reference/functions/generate-image-metadata
+export async function generateImageMetadata({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { title, og } = posts[Number(params.id)];
 
-export const runtime = "edge";
+  return [
+    {
+      contentType: "image/png",
+      size: { width: 48, height: 48 },
+      id: "small",
+    },
+    {
+      contentType: "image/png",
+      size: { width: 72, height: 72 },
+      id: "medium",
+    },
+  ];
+}
 
 export default async function Image({ params }: { params: { id: string } }) {
-  const postId = Number(params.id);
-  const { id, title, og } = posts[postId];
-
+  const { title, og } = posts[Number(params.id)];
   return new ImageResponse(
     (
+      // ImageResponse JSX element
       <div
         style={{
-          fontSize: 150,
-          background: `url('${og}')`,
+          fontSize: 184,
+          background: "black",
+          color: "white",
           width: "100%",
           height: "100%",
           display: "flex",
-          alignItems: "center",
           justifyContent: "center",
-          color: "white",
-          fontWeight: "bold",
+          alignItems: "center",
+          backgroundColor: "black",
+          backgroundImage: og,
         }}
       >
         {title}
       </div>
-    )
+    ),
+    // ImageResponse options
+    {
+      width: 1200,
+      height: 630,
+    }
   );
 }
