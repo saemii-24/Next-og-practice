9aaf3d750e3065c17519464a0749ef36967aea35
saemii-24 <hims2419@gmail.com>
2024-06-02 16:22:42 +0900

    Fix: third page url 변경

--- src/app/second/page.tsx
+++ src/app/second/page.tsx
@@ -1,3 +1,27 @@
+import ogImage from "./opengraph-image.jpg";
+
+export const metadata = {
+  metadataBase: new URL("https://next-og-practice.vercel.app"),
+  openGraph: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
+};
+
 export default function Second() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
--- src/app/third/page.tsx
+++ src/app/third/page.tsx
@@ -1,3 +1,23 @@
+import Image from "./opengraph-image";
+
+export const metadata = {
+  metadataBase: new URL("https://next-og-practice.vercel.app"),
+  openGraph: {
+    images: [
+      {
+        url: "https://next-og-practice.vercel.app/third/opengraph-image",
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: "https://next-og-practice.vercel.app/third/opengraph-image",
+      },
+    ],
+  },
+};
+
 export default function Third() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
