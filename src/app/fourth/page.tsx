c2975c885fd1b608bfd50d2b05344b1dd279f5cc
saemii-24 <hims2419@gmail.com>
2024-06-02 16:48:11 +0900

    Feat: fourth 오픈 그래프 테스트

--- src/app/fourth/page.tsx
+++ src/app/fourth/page.tsx
@@ -1,3 +1,36 @@
+export const metadata = {
+  metadataBase: new URL("https://next-og-practice.vercel.app"),
+  openGraph: {
+    title: "4번째 페이지 연습",
+    description: "4번째 페이지 오픈그래프 연습",
+    url: "https://next-og-practice.vercel.app",
+    siteName: "4번째 페이지 연습",
+    images: [
+      {
+        url: "/fourth/opengraph-image",
+      },
+    ],
+  },
+  twitter: {
+    title: "4번째 페이지 연습",
+    description: "4번째 페이지 오픈그래프 연습",
+    url: "https://next-og-practice.vercel.app",
+    siteName: "4번째 페이지 연습",
+    images: [
+      {
+        url: "/fourth/opengraph-image",
+      },
+    ],
+  },
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+      "de-DE": "/de-DE",
+    },
+  },
+};
+
 export default function Fourth() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
