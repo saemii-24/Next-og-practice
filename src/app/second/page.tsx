409e17e69806ed56d9be0ba51956a38df865802e
saemii-24 <hims2419@gmail.com>
2024-06-03 09:14:49 +0900

    Fix: second 페이지 오픈그래프 설정 변경

--- src/app/second/page.tsx
+++ src/app/second/page.tsx
@@ -4,9 +4,13 @@ console.log(ogImage);
 
 export const metadata = {
   metadataBase: new URL("https://next-og-practice.vercel.app"),
+  title: "2번째 페이지",
+  description: "2번째 페이지 설명",
   openGraph: {
     images: [
       {
+        title: "2번째 페이지 오픈그래프",
+        description: "2번째 페이지 설명 오픈그래프",
         url: ogImage.src,
         width: ogImage.width,
         height: ogImage.height,
@@ -16,6 +20,8 @@ export const metadata = {
   twitter: {
     images: [
       {
+        title: "2번째 페이지 트위터 카드",
+        description: "2번째 페이지 설명 트위터 카드",
         url: ogImage.src,
         width: ogImage.width,
         height: ogImage.height,
